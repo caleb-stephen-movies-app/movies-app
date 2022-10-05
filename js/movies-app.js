2af559572b09110fce00f97f7c0bf2b8488f6e98
Stephen Pflug <stephen@pflug.us>
2022-10-05 13:49:31 -0500

    setup movie list to populate as user enters title

--- js/movies-app.js
+++ js/movies-app.js
@@ -28,26 +28,76 @@ $(function() {
     }
 
     async function printAllMovies(moviesPromise) {
+        const cardDiv = $("#cardDiv");
+        cardDiv.empty();
         let movieData = await moviesPromise;
         console.log(movieData)
         movieData.forEach((movie) =>{
-
-
-            $('#cardDiv').append(`
-            <div>
-                <h2>Title: ${movie.title.toUpperCase()}</h2>
-                <p>Genre: ${movie.genre}</p>
-                <img src=${movie.poster}>
-                <p>Plot :${movie.plot}</p>
-                <p>Director: ${movie.director}</p>
-                <p>Actors: ${movie.actors}</p>
-                <p>Year: ${movie.year}</p>
-            </div>
-            
-            
-            `)
-        })
+            cardDiv.append(`
+                <div>
+                    <h2>Title: ${movie.title.toUpperCase()}</h2>
+                    <p>Genre: ${movie.genre}</p>
+                    <img src=${movie.poster}>
+                    <p>Plot :${movie.plot}</p>
+                    <p>Director: ${movie.director}</p>
+                    <p>Actors: ${movie.actors}</p>
+                    <p>Year: ${movie.year}</p>
+                </div>
+            `);
+        });
     }
+
+    async function addMovie(title) {
+        // get the movie info based on the information from TMDB api that is found with title from user
+        let movieSearchDataPromise = searchForMovie(title);
+        movieSearchDataPromise.then(results => {
+            console.log(results);
+        });
+
+        // show datalist so user can choose correct movie
+
+
+        // add movie to our db
+        // initialize post
+        // const postOptions = {
+        //     method: 'POST',
+        //     headers: {
+        //         'Content-Type' : 'application/json'
+        //     },
+        //     body: JSON.stringify(bookToPost)
+        // }
+
+        // re print all the movies
+    }
+
+    async function searchForMovie(title) {
+        console.log("inside getNewMovieData " + title);
+        try {
+            let response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${TMDB_KEY}&query=${title}&include_adult=false`);
+            let data = await response.json();
+            return data;
+        } catch(err) {
+            console.log(err);
+        }
+    }
+
+    async function populateMoviesList(title) {
+        let movieList = await searchForMovie(title).then(results => results);
+        $("#moviesList").empty();
+        movieList.results.forEach((movie, index) => {
+            if(index < 6) {
+                $("#moviesList").append(`
+                    <div class="col-2">
+                        <div class="card" data-movie-id="${movie.id}" style="height: 200px;">
+                            <h5 class="movie-list-item">${movie.title}</h5>
+                        </div>
+                    </div>
+                `);
+            }
+        });
+    }
+
+    // <option className="movie-list-item" data-movie-id="${movie.id}">${movie.original_title}</option>
 
 
     // populating our allmoviespromise so that when we get the results it will have a JSON file with all the movies
@@ -55,5 +105,26 @@ $(function() {
 
     // calling function to print all the movies on screen
     printAllMovies(allMoviesPromise);
+
+    // update movieList on space and enter
+    $("#addMovieText").keyup(e => {
+        if(e.key === "Enter" || e.key === " "){
+            populateMoviesList($("#addMovieText").val());
+        }
+    });
+
+    // update movieList on keyup
+    // $("#addMovieText").keyup(e => {
+    //     populateMoviesList($("#addMovieText").val())
+    // });
+
+    $(document.body).on("click", "#moviesList .card", function() {
+        console.log($(this).attr("data-movie-id"));
+    });
+
+    $("#addMovieBtn").click(() => {
+        console.log($("#addMovieText").data("movie-id"));
+    });
+
 
 });
