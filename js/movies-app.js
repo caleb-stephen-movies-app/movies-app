6f3a7cb2ce0a5acbfff97d4d31b39ceffab6363a
Stephen Pflug <stephen@pflug.us>
2022-10-07 13:31:58 -0500

    changed loading image

--- js/movies-app.js
+++ js/movies-app.js
@@ -19,9 +19,9 @@ $(function() {
         // Prints current movie database on screen and initializes all event listeners
         initialize() {
             // setTimeout just to show the loading screen for more than a split second. It can be removed for production
-            // setTimeout(() => {
+            setTimeout(() => {
             Print.allMovies(Get.allMovies());
-            // }, 5000);
+            }, 5000);
             Events.initialize();
         },
         // Function to change TMDB search to allow adult results
